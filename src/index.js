95b08ca5ad0aabd3b0258f356e167c1ecbf2cd22
Marc Bernardoni <marc.bernardoni@gmail.com>
2022-11-11 18:20:49 +0100

    intégration de Husky

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log('Hello World!')
+
+const addition = (a, b) => a + b
+
+const numberOne = 12
+const numberTwo = 13
+
+const resultat = addition(numberOne, numberTwo)
+console.log('resultat:', resultat)
